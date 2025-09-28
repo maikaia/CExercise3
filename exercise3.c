ac610295b03171d2732fef10203ba9ae48a03ead
maikaia <nolle_olsson_@hotmail.com>
2025-09-28 14:50:16 +0200

    Added function info() with hard coded person and vehicle lists

--- exercise3.c
+++ exercise3.c
@@ -1,9 +1,31 @@
+// Nils-Olov Olsson & Samuel Grafström
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
 
+// Constants
+
 #define SIZE 100
+#define LIST_SIZE 10
+
+// Structures
+
+struct Person {
+    char name[SIZE];
+    int age;
+};
+typedef struct Person person_t;
+
+struct Vehicle {
+    char type[SIZE];
+    char brand[SIZE];
+    char license_plate[SIZE];
+    person_t owner;
+};
+typedef struct Vehicle vehicle_t;
+
+// Functions
 
 void print_menu()
 {
@@ -19,17 +41,66 @@ void print_menu()
     printf("-----------------------------\n");
 }
 
+int add_vehicle()
+{
+    int bla;
+    printf("Add vehicle\n"); 
+    scanf("%i", &bla);
+    printf("%i is now registered!\n", bla);
+}
+
+int info()
+{
+    printf("You choose info about vehicle\n");
+    printf("Please enter number to show vehicle in responding place\n");
+    int input;
+    scanf("%i", &input);
+
+    person_t person[LIST_SIZE] = 
+    {
+        {"Nils-Olov Olsson", 24}, 
+        {"Samuel Grafström", 23},
+        {"3", 23},
+        {"4", 23},
+        {"5", 23},
+        {"6", 23},
+        {"7", 23},
+        {"8", 23},
+        {"9", 23},
+        {"10", 23},
+    };
+    vehicle_t car[LIST_SIZE] = 
+    {
+        {"307 CC", "Peugeot", "WUD786", person[input-1]},
+        {"A3", "Audi", "ATO717", person[input-1]},
+        {"307 CC", "BMW", "3", person[input-1]},
+        {"307 CC", "Volvo", "4", person[input-1]},
+        {"307 CC", "Mercedes", "5", person[input-1]},
+        {"307 CC", "Volkswagen", "6", person[input-1]},
+        {"307 CC", "Kia", "7", person[input-1]},
+        {"307 CC", "Mazda", "8", person[input-1]},
+        {"307 CC", "Tesla", "9", person[input-1]},
+        {"307 CC", "Fiat", "10", person[input-1]},
+    };
+
+    printf("Info about vehicle on position %i:\n", input);
+    printf("Type: %s\n", car[input-1].type);
+    printf("Brand: %s\n", car[input-1].brand);
+    printf("License plate: %s\n", car[input-1].license_plate);
+    printf("Owner: %s\n", car[input-1].owner.name);
+}
+
 int handle_user_input()
 {
     char buffer[SIZE];
     int temp;
-    scanf("%s\n", buffer);
+    scanf("%s", buffer);
     if(isalpha(buffer[0]) || strlen(buffer) > 1)
     {
         return -1;
     }
     temp = atoi(buffer);
-
+    
     return temp;
 }
 
@@ -37,13 +108,15 @@ void act_upon_input(int choice)
 {
     switch(choice)
     {
-        case 1: printf("Add vehicle\n"); 
+        case 1: 
+            add_vehicle();
         break;
         case 2: printf("Remove vehicle\n");
         break;
         case 3: printf("Sort\n");
         break;
-        case 4: printf("Info\n");
+        case 4: 
+            info();
         break;
         case 5: printf("Show all\n");
         break;
@@ -53,8 +126,13 @@ void act_upon_input(int choice)
         break;
         case 0: printf("Exiting program!\n");
         break;
+        default: printf("Not a valid input, try using a number you see on the menu\n");
+        break;
     }
 }
+
+
+// Main
 
 int main()
 {
